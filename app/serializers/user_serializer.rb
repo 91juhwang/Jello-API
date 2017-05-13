ba0c2fe91ea8908c778bfff61b44191ce1e79fd6
91juhwang <91juhwang@gmail.com>
2017-05-13 18:00:52 -0400

    Add active_model_serailizer config.adapter :json in the initializer to include the root object in the response

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :gravatar_url, :admin, :created_at, :updated_at
+  attributes :mailto_url
+
+  def mailto_url
+    "mailto:#{ object.email }"
+  end
 end
